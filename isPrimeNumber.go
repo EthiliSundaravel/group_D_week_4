271d797e20032d3b3c7a100089f8b33ce225a85c
DESKTOP-RHMUDV9\Ethili Sundaravel <ethilispsdpe@gmail.com>
2024-09-27 17:59:20 -0400

    500236470 adding isPrimeNumber function

--- isPrimeNumber.go
+++ isPrimeNumber.go
@@ -5,12 +5,11 @@ import "math"
 
 func isPrimeNumber(num int) bool{
 	if num <= 1 {
-		return false // 0 and 1 are not prime
-	}
+		return false 
 	for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
 		if num%i == 0 {
-			return false // Not prime
+			return false 
 		}
 	}
-	return true // Is prime
+	return true 
 }
--- main.go
+++ main.go
@@ -13,16 +13,21 @@ func main() {
     var num int
     var operation string
 
-    fmt.Print("Enter the number: ")
-    fmt.Scanln(&num)
-    
-
     fmt.Println("Choose an operation : \n 1 : count digits in a number \n 2 : check for palindrome \n 3 : check for prime number \n 4 : factorial of a number")
     fmt.Scanln(&operation)
+
+    fmt.Print("Enter the number: ")
+    fmt.Scanln(&num)
 
     switch operation {
     case "1":
         fmt.Printf("total digits in a number %d = %d\n", num, countDigits(num))
+    case "3":
+        if isPrimeNumber(num) {
+            fmt.Printf("%d is a prime number.\n", num)
+        } else {
+            fmt.Printf("%d is not a prime number.\n", num)
+        }
     
     default:
         fmt.Println("Invalid operation! Please choose from 1 to 4")
